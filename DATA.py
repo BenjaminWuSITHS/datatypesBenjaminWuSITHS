6d2ec43b3d2c2465d4027c8e3dd53228c4c7eda8
BenjaminWuSITHS <benjaminw131@nycstudents.net>
2026-09-18 09:20:19 -0400

    Update DATA.py

--- DATA.py
+++ DATA.py
@@ -16,8 +16,12 @@ z = y[0] #1st word
 print(y)
 print(z)
 
-day_of_week = input("what day is it? ")
-if day_of_week == "Friday" or "friday": #python is case sensitive 
-    print("correct")
-else:
-    print("incorrect")
+#Using the "input" method in Python, ask a user to input a sentence. 
+# Then develop a function that accepts a the user input and will tell you how many words are in that string. 
+# First write out your plan in Pseudo-code using comments. Then craft the function.
+# wrap int() around to turn string into integers
+def countwords():
+    words = input("give sentence") # asks user a question, input always a string
+    word = words.upper
+
+countwords()
